07fc6b705f5604dbbc3c838a282ac5cb3a1ec5a9
LancelotOzerny <lancelot.ozernuy@gmail.com>
2026-03-26 02:23:21 +0300

    Написал объект простой пули и реализовал стрельбу космического корабля.

--- game.js
+++ game.js
@@ -11,49 +11,6 @@ document.addEventListener('DOMContentLoaded', async () => {
         });
 });
 
-class Hero extends GameObject
-{
-    init()
-    {
-        this.sprite = new SpriteComponent({
-            sprite: 'sheep',
-            width: 256,
-            height: 256
-        });
-        this.sprite.color = 'rgba(255, 255, 0, 1)';
-        this.bindComponent(this.sprite);
-
-        super.init();
-    }
-
-    moveSpeed = 1_000;
-    moveX = 0;
-
-    start()
-    {
-        super.start();
-        this.transform.position.set(
-            Engine.instance.canvas.width / 2 - this.sprite.width / 2,
-            Engine.instance.canvas.height - this.sprite.height - 50
-        )
-    }
-
-    update(deltaTime)
-    {
-        if (Engine.instance.input.isKeyDown('KeyD')) this.moveX = 1;
-        else if (Engine.instance.input.isKeyDown('KeyA')) this.moveX = -1;
-        else this.moveX = 0;
-
-        if (this.transform.position.x > Engine.instance.options.width - this.sprite.width && this.moveX > 0 ||
-            this.transform.position.x < 0 && this.moveX < 0)
-        {
-            this.moveX = 0;
-        }
-
-        this.transform.translate(this.moveX * this.moveSpeed * deltaTime, 0);
-    }
-}
-
 class Game
 {
     engine = Engine.instance;
@@ -82,5 +39,77 @@ class Game
     start()
     {
         this.engine.startGameLoop();
+    }
+}
+
+class Hero extends GameObject
+{
+    init()
+    {
+        this.sprite = new SpriteComponent({
+            sprite: 'sheep',
+            width: 256,
+            height: 256
+        });
+        this.bindComponent(this.sprite);
+
+        super.init();
+    }
+
+    moveSpeed = 1_000;
+    moveX = 0;
+
+    start()
+    {
+        super.start();
+        this.transform.position.set(
+            Engine.instance.canvas.width / 2 - this.sprite.width / 2,
+            Engine.instance.canvas.height - this.sprite.height - 50
+        )
+    }
+
+    update(deltaTime)
+    {
+        if (Engine.instance.input.isKeyDown('KeyD')) this.moveX = 1;
+        else if (Engine.instance.input.isKeyDown('KeyA')) this.moveX = -1;
+        else this.moveX = 0;
+
+        if (Engine.instance.input.isKeyPressed('Space'))
+        {
+            let bullet = new Bullet();
+            bullet.transform.position.set(
+                this.transform.position.x + this.sprite.width / 2 - bullet.sprite.width / 2,
+                this.transform.position.y - bullet.sprite.height / 2,
+            );
+            this.parent.gameObjects.append(bullet);
+        }
+
+        if (this.transform.position.x > Engine.instance.options.width - this.sprite.width && this.moveX > 0 ||
+            this.transform.position.x < 0 && this.moveX < 0)
+        {
+            this.moveX = 0;
+        }
+
+        this.transform.translate(this.moveX * this.moveSpeed * deltaTime, 0);
+    }
+}
+
+class Bullet extends GameObject
+{
+    speed = 10;
+    init()
+    {
+        this.sprite = new SpriteComponent({
+            width: 6,
+            height: 12
+        });
+        this.bindComponent(this.sprite);
+        super.init();
+    }
+
+    update(deltaTime)
+    {
+        this.transform.translate(0, -this.speed);
+        super.update(deltaTime);
     }
 }
