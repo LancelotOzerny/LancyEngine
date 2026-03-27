94acb268394c91dcdab274a71d258efd37544fa7
LancelotOzerny <lancelot.ozernuy@gmail.com>
2026-03-28 01:21:50 +0300

    Внес правки в систему коллизий

--- src/scripts/components/collider-component.js
+++ src/scripts/components/collider-component.js
@@ -1,7 +1,38 @@
 class ColliderComponent extends BaseComponent
 {
-    get isCollision() { return this.collisions.length }
     collisions = [];
+    contacts = [];
+    touching = {
+        left: false,
+        right: false,
+        top: false,
+        bottom: false,
+    };
+
+    offset = new Vector2();
+    isTrigger = false;
+    isStatic = false;
+    isEnabled = true;
+    layer = 0;
+    mask = null;
+
+    constructor(params = {})
+    {
+        super(params);
+
+        this.isTrigger = params.isTrigger ?? false;
+        this.isStatic = params.isStatic ?? false;
+        this.isEnabled = params.isEnabled ?? true;
+        this.layer = params.layer ?? 0;
+        this.mask = Array.isArray(params.mask) ? [...params.mask] : null;
+
+        this.offset.set(params.offsetX ?? 0, params.offsetY ?? 0);
+    }
+
+    get isCollision()
+    {
+        return this.collisions.length;
+    }
 
     get collision()
     {
@@ -10,12 +41,12 @@ class ColliderComponent extends BaseComponent
 
     init()
     {
-        let sprite = this.parent.findComponent(SpriteComponent);
+        const sprite = this.parent.findComponent(SpriteComponent);
 
         if (sprite)
         {
-            if (typeof(this.params.width) === 'undefined') this.params.width = sprite.width;
-            if (typeof(this.params.height) === 'undefined') this.params.height = sprite.height;
+            if (this.params.width === undefined) this.params.width = sprite.width;
+            if (this.params.height === undefined) this.params.height = sprite.height;
         }
 
         this.params.width ??= 150;
@@ -24,26 +55,118 @@ class ColliderComponent extends BaseComponent
         CollisionSystem.instance.append(this);
     }
 
-    checkCollision(collider2)
+    resetFrameState()
     {
-        let info1 = this.getInfoForCheck();
-        let info2 = collider2.getInfoForCheck();
+        this.contacts.length = 0;
+        this.collisions.length = 0;
 
-        let leftCollision = info1.posX + info1.width >= info2.posX;
-        let rightCollision = info1.posX <= info2.posX + info2.width;
-        let topCollision = info1.posY + info1.height >= info2.posY;
-        let bottomCollision = info1.posY <= info2.posY + info2.height;
-
-        return leftCollision && rightCollision && topCollision && bottomCollision;
+        this.touching.left = false;
+        this.touching.right = false;
+        this.touching.top = false;
+        this.touching.bottom = false;
     }
 
-    getInfoForCheck()
+    canCollideWith(other)
+    {
+        if (!(other instanceof ColliderComponent)) return false;
+        if (!this.isEnabled || !other.isEnabled) return false;
+        if (this.parent === other.parent) return false;
+
+        const selfAllows = this.mask === null || this.mask.includes(other.layer);
+        const otherAllows = other.mask === null || other.mask.includes(this.layer);
+
+        return selfAllows && otherAllows;
+    }
+
+    getBounds(
+        posX = this.parent.transform.position.x,
+        posY = this.parent.transform.position.y
+    )
+    {
+        const left = posX + this.offset.x;
+        const top = posY + this.offset.y;
+        const width = this.params.width;
+        const height = this.params.height;
+
+        return {
+            left,
+            top,
+            right: left + width,
+            bottom: top + height,
+            width,
+            height,
+            centerX: left + width / 2,
+            centerY: top + height / 2,
+        };
+    }
+
+    getInfoForCheck(
+        posX = this.parent.transform.position.x,
+        posY = this.parent.transform.position.y
+    )
     {
         return {
-            posX: this.parent.transform.position.x,
-            posY: this.parent.transform.position.y,
+            posX: posX + this.offset.x,
+            posY: posY + this.offset.y,
             width: this.params.width,
             height: this.params.height,
+        };
+    }
+
+    getOverlap(
+        other,
+        selfPosX = this.parent.transform.position.x,
+        selfPosY = this.parent.transform.position.y,
+        otherPosX = other.parent.transform.position.x,
+        otherPosY = other.parent.transform.position.y
+    )
+    {
+        const a = this.getBounds(selfPosX, selfPosY);
+        const b = other.getBounds(otherPosX, otherPosY);
+
+        const overlapX = Math.min(a.right, b.right) - Math.max(a.left, b.left);
+        const overlapY = Math.min(a.bottom, b.bottom) - Math.max(a.top, b.top);
+
+        return {
+            intersects: overlapX > 0 && overlapY > 0,
+            overlapX: Math.max(0, overlapX),
+            overlapY: Math.max(0, overlapY),
+            self: a,
+            other: b,
+        };
+    }
+
+    checkCollision(
+        other,
+        selfPosX = this.parent.transform.position.x,
+        selfPosY = this.parent.transform.position.y,
+        otherPosX = other.parent.transform.position.x,
+        otherPosY = other.parent.transform.position.y
+    )
+    {
+        return this.getOverlap(other, selfPosX, selfPosY, otherPosX, otherPosY).intersects;
+    }
+
+    addContact(other, side, normalX, normalY, overlapX = 0, overlapY = 0, isTrigger = false)
+    {
+        this.contacts.push({
+            other,
+            side,
+            normalX,
+            normalY,
+            overlapX,
+            overlapY,
+            isTrigger,
+        });
+
+        if (!isTrigger && side && this.touching[side] !== undefined)
+        {
+            this.touching[side] = true;
+        }
+
+        if (!this.collisions.includes(other))
+        {
+            this.collisions.push(other);
         }
     }
 
--- src/scripts/components/physic-component.js
+++ src/scripts/components/physic-component.js
@@ -3,38 +3,178 @@ class PhysicComponent extends BaseComponent
     gravityScale = 1;
     mass = 1;
     drag = 0;
+    useGravity = true;
+    isGrounded = false;
 
     velocity = new Vector2();
     force = new Vector2();
+    prevPosition = new Vector2();
 
     start()
     {
         this.velocity.set(0, 0);
         this.force.set(0, 0);
+        this.prevPosition.set(
+            this.parent.transform.position.x,
+            this.parent.transform.position.y
+        );
     }
 
-    applyForce(forceX, forceY)
+    applyForce(x, y)
     {
-        this.force.x += forceX;
-        this.force.y += forceY;
+        this.force.x += x;
+        this.force.y += y;
     }
 
-    update(deltaTime)
+    update(dt)
     {
         const transform = this.parent.transform;
+        const collider = this.parent.findComponent(ColliderComponent);
 
-        let accelerationX = (this.force.x - this.velocity.x * this.drag) / this.mass;
-        let accelerationY = (this.force.y - this.velocity.y * this.drag) / this.mass;
+        this.prevPosition.set(transform.position.x, transform.position.y);
 
-        const gravityForce = 9.81 * this.gravityScale;
-        accelerationY += gravityForce;
+        if (collider)
+        {
+            collider.resetFrameState();
+        }
 
-        this.velocity.x += accelerationX * deltaTime;
-        this.velocity.y += accelerationY * deltaTime;
+        const safeMass = this.mass <= 0 ? 0.0001 : this.mass;
 
-        transform.position.x += this.velocity.x * deltaTime;
-        transform.position.y += this.velocity.y * deltaTime;
+        const ax = (this.force.x - this.velocity.x * this.drag) / safeMass;
+        let ay = (this.force.y - this.velocity.y * this.drag) / safeMass;
 
+        if (this.useGravity)
+        {
+            ay += 9.81 * this.gravityScale;
+        }
+
+        this.velocity.x += ax * dt;
+        this.velocity.y += ay * dt;
+
+        if (!collider || collider.isTrigger)
+        {
+            transform.position.x += this.velocity.x * dt;
+            transform.position.y += this.velocity.y * dt;
+            this.force.set(0, 0);
+            return;
+        }
+
+        this.moveAxisX(dt, collider);
+
+        this.isGrounded = false;
+        this.moveAxisY(dt, collider);
+
+        this.collectTriggerContacts(collider);
         this.force.set(0, 0);
+    }
+
+    moveAxisX(dt, collider)
+    {
+        const transform = this.parent.transform;
+        transform.position.x += this.velocity.x * dt;
+
+        const overlaps = CollisionSystem.instance.getOverlaps(collider);
+
+        for (const entry of overlaps)
+        {
+            const other = entry.collider;
+            const info = entry.info;
+
+            if (other.isTrigger || info.overlapX <= 0 || info.overlapY <= 0)
+            {
+                continue;
+            }
+
+            const selfBounds = collider.getBounds();
+            const otherBounds = info.other;
+
+            if (this.velocity.x > 0)
+            {
+                transform.position.x = otherBounds.left - collider.offset.x - selfBounds.width;
+                collider.addContact(other, "right", -1, 0, info.overlapX, info.overlapY, false);
+            }
+            else if (this.velocity.x < 0)
+            {
+                transform.position.x = otherBounds.right - collider.offset.x;
+                collider.addContact(other, "left", 1, 0, info.overlapX, info.overlapY, false);
+            }
+            else if (selfBounds.centerX < otherBounds.centerX)
+            {
+                transform.position.x = otherBounds.left - collider.offset.x - selfBounds.width;
+                collider.addContact(other, "right", -1, 0, info.overlapX, info.overlapY, false);
+            }
+            else
+            {
+                transform.position.x = otherBounds.right - collider.offset.x;
+                collider.addContact(other, "left", 1, 0, info.overlapX, info.overlapY, false);
+            }
+
+            this.velocity.x = 0;
+        }
+    }
+
+    moveAxisY(dt, collider)
+    {
+        const transform = this.parent.transform;
+        transform.position.y += this.velocity.y * dt;
+
+        const overlaps = CollisionSystem.instance.getOverlaps(collider);
+
+        for (const entry of overlaps)
+        {
+            const other = entry.collider;
+            const info = entry.info;
+
+            if (other.isTrigger || info.overlapX <= 0 || info.overlapY <= 0)
+            {
+                continue;
+            }
+
+            const selfBounds = collider.getBounds();
+            const otherBounds = info.other;
+
+            if (this.velocity.y > 0)
+            {
+                transform.position.y = otherBounds.top - collider.offset.y - selfBounds.height;
+                this.isGrounded = true;
+                collider.addContact(other, "bottom", 0, -1, info.overlapX, info.overlapY, false);
+            }
+            else if (this.velocity.y < 0)
+            {
+                transform.position.y = otherBounds.bottom - collider.offset.y;
+                collider.addContact(other, "top", 0, 1, info.overlapX, info.overlapY, false);
+            }
+            else if (selfBounds.centerY < otherBounds.centerY)
+            {
+                transform.position.y = otherBounds.top - collider.offset.y - selfBounds.height;
+                this.isGrounded = true;
+                collider.addContact(other, "bottom", 0, -1, info.overlapX, info.overlapY, false);
+            }
+            else
+            {
+                transform.position.y = otherBounds.bottom - collider.offset.y;
+                collider.addContact(other, "top", 0, 1, info.overlapX, info.overlapY, false);
+            }
+
+            this.velocity.y = 0;
+        }
+    }
+
+    collectTriggerContacts(collider)
+    {
+        const overlaps = CollisionSystem.instance.getOverlaps(collider);
+
+        for (const entry of overlaps)
+        {
+            const other = entry.collider;
+            const info = entry.info;
+
+            if (!other.isTrigger)
+            {
+                continue;
+            }
+
+            collider.addContact(other, null, 0, 0, info.overlapX, info.overlapY, true);
+        }
     }
 }
