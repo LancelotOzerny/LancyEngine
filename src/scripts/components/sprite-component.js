7acbd8d60fd5148a7bb30b01f0a1b15b69a2f186
LancelotOzerny <lancelot.ozernuy@gmail.com>
2026-03-28 13:01:27 +0300

    Новый космический шутер от нейросети на моем движке

--- src/scripts/components/sprite-component.js
+++ src/scripts/components/sprite-component.js
@@ -3,69 +3,68 @@ class SpriteComponent extends BaseComponent
     sprite = null;
     color = null;
     alpha = 1;
-    blendMode = 'overlay';
-
+    blendMode = "overlay";
     _width = 150;
     _height = 150;
-
     constructor(params = {})
     {
         super(params);
-
-        this.width = this.params.width ?? 150;
-        this.height = this.params.height ?? 150;
+        this._width = this.params.width ?? 150;
+        this._height = this.params.height ?? 150;
     }
-
-    get width() { return this._width; }
-    set width(value)
+    get
+    width()
+    {
+        return this._width;
+    }
+    set
+    width(value)
     {
         this._width = value;
-        if (this.sprite)
-        {
-            this.sprite.width = value;
-        }
     }
-    get height() { return this._height; }
-    set height(value)
+    get
+    height()
+    {
+        return this._height;
+    }
+    set
+    height(value)
     {
         this._height = value;
-        if (this.sprite)
-        {
-            this.sprite.height = value;
-        }
     }
-
     init()
     {
         if (this.params.sprite)
         {
-            this.sprite = Engine.instance.assets.get(this.params.sprite);
-            this.width = this.params.width ?? this.sprite.width;
-            this.height = this.params.height ?? this.sprite.height;
+            const asset = Engine.instance.assets?.get(this.params.sprite);
+            if (!asset)
+            {
+                throw new Error(`Sprite asset "${this.params.sprite}" not found in Engine.instance.assets`);
+            }
+            this.sprite = asset;
+            this._width = this.params.width ?? asset.naturalWidth ?? asset.width ?? 150;
+            this._height = this.params.height ?? asset.naturalHeight ?? asset.height ?? 150;
         }
-
-        this.width = this.params.width ?? this.width;
-        this.height = this.params.height ?? this.height;
+        else
+        {
+            this._width = this.params.width ?? this._width;
+            this._height = this.params.height ?? this._height;
+        }
     }
-
     render(ctx)
     {
-        let pos = this.parent.transform.position;
-
+        const pos = this.parent.transform.position;
         ctx.save();
         ctx.translate(pos.x + this.width / 2, pos.y + this.height / 2);
         ctx.rotate(this.parent.transform.rotation * Math.PI / 180);
-
         ctx.globalAlpha = this.alpha;
-        ctx.fillStyle = this.color;
-
         if (this.sprite)
         {
             ctx.drawImage(this.sprite, -this.width / 2, -this.height / 2, this.width, this.height);
-
             if (this.color)
             {
                 ctx.save();
+                ctx.fillStyle = this.color;
                 ctx.globalCompositeOperation = this.blendMode;
                 ctx.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
                 ctx.restore();
@@ -73,6 +72,7 @@ class SpriteComponent extends BaseComponent
         }
         else
         {
+            ctx.fillStyle = this.color ?? "#ff00ff";
             ctx.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
         }
         ctx.restore();
