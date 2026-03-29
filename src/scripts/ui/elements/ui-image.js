be805715fa0d5d98cbfc745aed47a44b1b3f4c4b
LancelotOzerny <lancelot.ozernuy@gmail.com>
2026-03-29 22:51:57 +0300

    Написал элемент ImageUI

--- src/scripts/ui/elements/ui-image.js
+++ src/scripts/ui/elements/ui-image.js
@@ -7,5 +7,21 @@ class UiImage extends UiElement
             baseClass: 'ui-image',
             ...options
         });
+
+        this.width = this.options.width ?? '150';
+        this.height = this.options.height ?? '150';
+        this.html.setAttribute('src', this.options.src ?? 'none');
+    }
+
+    set width(value)
+    {
+        this.options.width = value;
+        this.html.style.width = value + 'px';
+    }
+
+    set height(value)
+    {
+        this.options.height = value;
+        this.html.style.height = value + 'px';
     }
 }
